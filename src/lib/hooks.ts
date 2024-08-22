8f02a577e89edf301b00e9b07336105bbc0c48aa
swapnils118 <swapnil118@icloud.com>
2024-08-22 18:28:23 -0400

    Types added for React-Query fetch function

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,7 +1,68 @@
 import { useEffect, useState } from "react";
-import { JobItem } from "./types";
+import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
+
+// export function useJobItem(id: number | null) {
+//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+//   const [isLoading, setIsLoading] = useState(false);
+//     console.log(id);
+
+//   useEffect(() => {
+//     if (!id) return;
+
+//     const fetchData = async () => {
+//       setIsLoading(true);
+//       const response = await fetch(`${BASE_API_URL}/${id}`);
+//       const data = await response.json();
+//       setIsLoading(false);
+//       //   console.log(data);
+//       setJobItem(data.jobItem);
+//     };
+
+//     fetchData();
+//   }, [id]);
+
+//   return { jobItem, isLoading } as const;
+// }
+export function useJobItem(id: number | null) {
+  type JobItemApiResponse = {
+    public: boolean;
+    jobItem: JobItemExpanded;
+  };
+  const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+    const response = await fetch(`${BASE_API_URL}/${id}`);
+    const data = await response.json();
+    return data;
+  };
+  const { data, isLoading } = useQuery(
+    ["job-item", id],
+    () => (id ? fetchJobItem(id) : null),
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: () => {},
+    }
+  );
+  console.log(data);
+  const jobItem = data?.jobItem;
+
+  return { jobItem, isLoading } as const;
+}
+
+export function useDebounce<T>(value: T, delay = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
 
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -46,61 +107,4 @@ export function useJobItems(searchText: string) {
   }, [searchText]);
 
   return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
-}
-
-// export function useJobItem(id: number | null) {
-//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-//   const [isLoading, setIsLoading] = useState(false);
-//     console.log(id);
-
-//   useEffect(() => {
-//     if (!id) return;
-
-//     const fetchData = async () => {
-//       setIsLoading(true);
-//       const response = await fetch(`${BASE_API_URL}/${id}`);
-//       const data = await response.json();
-//       setIsLoading(false);
-//       //   console.log(data);
-//       setJobItem(data.jobItem);
-//     };
-
-//     fetchData();
-//   }, [id]);
-
-//   return { jobItem, isLoading } as const;
-// }
-
-export function useJobItem(id: number | null) {
-  const { data, isLoading } = useQuery(
-    ["job-item", id],
-    async () => {
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      return data;
-    },
-    {
-      staleTime: 1000 * 60 * 60,
-      refetchOnWindowFocus: false,
-      retry: false,
-      enabled: Boolean(id),
-      onError: () => {},
-    }
-  );
-  console.log(data);
-  const jobItem = data?.jobItem;
-
-  return { jobItem, isLoading } as const;
-}
-
-export function useDebounce<T>(value: T, delay = 500): T {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(() => {
-    const timerId = setTimeout(() => setDebouncedValue(value), delay);
-
-    return () => clearTimeout(timerId);
-  }, [value, delay]);
-
-  return debouncedValue;
 }
