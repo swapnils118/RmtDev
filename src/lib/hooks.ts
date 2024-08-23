d1e747c3cd9abbf2051ac34bcf72e54f4b984719
swapnils118 <swapnil118@icloud.com>
2024-08-23 16:18:18 -0400

    Error handling in React-Query

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -33,6 +33,11 @@ type JobItemApiResponse = {
 export function useJobItem(id: number | null) {
   const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
     const response = await fetch(`${BASE_API_URL}/${id}`);
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.description);
+    }
+
     const data = await response.json();
     return data;
   };
@@ -45,7 +50,9 @@ export function useJobItem(id: number | null) {
       refetchOnWindowFocus: false,
       retry: false,
       enabled: Boolean(id),
-      onError: () => {},
+      onError: (error) => {
+        console.log(error);
+      },
     }
   );
   console.log(data);
