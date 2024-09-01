382dbb507c5f2796c247b018e3f5674b56b1b201
swapnils118 <swapnil118@icloud.com>
2024-09-01 15:30:47 -0400

    Introduced useRef for BookmarksPopover

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,16 +1,18 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       if (
         e.target instanceof HTMLElement &&
-        !e.target.closest(".bookmarks-btn") &&
-        !e.target.closest(".bookmarks-popover")
+        !buttonRef.current?.contains(e.target) &&
+        !popoverRef.current?.contains(e.target)
       ) {
         setIsOpen(false);
       }
@@ -26,13 +28,14 @@ export default function BookmarksButton() {
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className="bookmarks-btn"
       >
         Bookmarks <TriangleDownIcon />
       </button>
 
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,11 +1,14 @@
+import { forwardRef } from "react";
 import { useBookmarksContext } from "../contexts/BookmarksContextProvider";
 import JobList from "./JobList";
 
-export default function BookmarksPopover() {
+const BookmarksPopover = forwardRef(function (props, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
   return (
-    <div className="bookmarks-popover">
+    <div ref={popoverRef} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
     </div>
   );
-}
+});
+
+export default BookmarksPopover;
