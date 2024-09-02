b28be19bede0027a20da986ef57aa03d1024a62e
swapnils118 <swapnil118@icloud.com>
2024-09-02 17:40:35 -0400

    Types added for custom hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -202,14 +202,17 @@ export function useLocalStorage<T>(
 
 // ------------------------------------
 
-export function useOnClickOutside(refs, handler) {
+export function useOnClickOutside(
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void
+) {
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       if (
-        e.target instanceof HTMLElement &&
+        // e.target instanceof HTMLElement &&
         // !buttonRef.current?.contains(e.target) &&
         // !popoverRef.current?.contains(e.target)
-        refs.every((ref) => !ref.current?.contains(e.target))
+        refs.every((ref) => !ref.current?.contains(e.target as Node))
       ) {
         handler();
       }
