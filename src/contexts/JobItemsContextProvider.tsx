f91e8aa1999518f5967ea209b081fc4b0decf87f
swapnils118 <swapnil118@icloud.com>
2024-09-05 16:41:40 -0400

    JobItemsContextProvider modified with useMemo on contextValue

--- src/contexts/JobItemsContextProvider.tsx
+++ src/contexts/JobItemsContextProvider.tsx
@@ -69,20 +69,34 @@ export default function JobItemsContextProvider({
     setCurrentPage(1);
     setSortBy(newSortBy);
   };
+
+  const contextValue = useMemo(
+    () => ({
+      jobItems,
+      jobItemsSortedAndSliced,
+      isLoading,
+      totalNumberOfResults,
+      totalNumberOfPages,
+      currentPage,
+      sortBy,
+      handleChangePage,
+      handleChangeSortBy,
+    }),
+    [
+      jobItems,
+      jobItemsSortedAndSliced,
+      isLoading,
+      totalNumberOfResults,
+      totalNumberOfPages,
+      currentPage,
+      sortBy,
+      handleChangePage,
+      handleChangeSortBy,
+    ]
+  );
+
   return (
-    <JobItemsContext.Provider
-      value={{
-        jobItems,
-        jobItemsSortedAndSliced,
-        isLoading,
-        totalNumberOfResults,
-        totalNumberOfPages,
-        currentPage,
-        sortBy,
-        handleChangePage,
-        handleChangeSortBy,
-      }}
-    >
+    <JobItemsContext.Provider value={contextValue}>
       {children}
     </JobItemsContext.Provider>
   );
